0231269e22a7af3f2fc26db7eaab66fd1ecaf9b4
Shawn Lu <65582158+ShawnLu13@users.noreply.github.com>
2024-04-20 23:52:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
 // 首页+表单路由
